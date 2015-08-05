2222a03cc5acc8aa9183a9af90f0ed134bf5976e
Marco Wise <mrmarco@ichiko.local>
2015-08-05 13:11:30 -0700

    added some new apps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,18 @@
 ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+
+# Install Homebrew (http://brew.sh/)
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install homebrew-cask (http://caskroom.io/)
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+# Install Google Chrome
+brew cask install google-chrome
+
+# Install Slack
+brew cask install slack
+
+# Install TaskPaper
+brew cask install taskpaper
