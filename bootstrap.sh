cd5505286d41426853c73e4b238a7df7e96d9163
Marco Wise <marco.wise@stanford.edu>
2015-08-19 19:02:11 -0700

    added Sublime Text

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,3 +16,14 @@ brew cask install slack
 
 # Install TaskPaper
 brew cask install taskpaper
+
+# Install Sublime Text 3
+brew case install sublime-text3
+
+## OSX Settings
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string “png”
